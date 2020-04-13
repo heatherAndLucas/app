de32049ff2a0c4841b019e2f1d2d5f1b27d9d45b
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2020-04-13 17:56:50 -0400

    added pseudo code

---
+++ script.js
@@ -0,0 +1,20 @@
+// Create app namespace to hold all methods
+const app = {};
+
+// When user clicks the start button, make AJAX request to get questions
+
+// Display questions to user
+// Randomly display options of correct and incorrect answers to user
+
+// When user clicks submit, check user answers against correct answers
+// Verify all questions have been answered
+
+// Display score/results along with button to play again
+
+// Start app
+app.init = function() {};
+
+// Document ready
+$(function() {
+	app.init();
+});
